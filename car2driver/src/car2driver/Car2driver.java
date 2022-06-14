7208c3bd49a59e90a60bf6086cee37137c31cf3e
Kevin Bell <bellKevin@pm.me>
2022-06-14 18:55:48 +0000

    Update Car2driver.java

--- car2driver/src/car2driver/Car2driver.java
+++ car2driver/src/car2driver/Car2driver.java
@@ -16,6 +16,18 @@ public class Car2driver {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Car2 nathanCar = new Car2();
+        Car2 nickCar = new Car2();
+
+        nathanCar.setMake("Audi");
+        nathanCar.setYear(2012);
+        nathanCar.setColor("green");
+        nickCar.setMake("Audi");
+        nickCar.setYear(2012);
+        nickCar.setColor("green");
+        if (nathanCar.equals(nickCar)) {
+            System.out.println("Cars have identical features");
+        }
     }
-    
+
 }
