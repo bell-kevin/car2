044f49bd9d76e4f4fdcd14aef62909dcae6c9d2f
Kevin Bell <bellKevin@pm.me>
2022-06-14 18:56:32 +0000

    Update Car2driver.java

--- car2driver/src/car2driver/Car2driver.java
+++ car2driver/src/car2driver/Car2driver.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package car2driver;
 
-/**
- *
- * @author 4800590195
- */
 public class Car2driver {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         Car2 nathanCar = new Car2();
@@ -29,5 +17,4 @@ public class Car2driver {
             System.out.println("Cars have identical features");
         }
     }
-
 }
